6c576a8b964117bfecb1fdc43602cac952c30474
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:21:44 +0530

    Add project files.

---
+++ ThrowVsThrowEx/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowVsThrowEx
+{
+    internal class Program
+    {
+        static void Method1()
+        {
+            try
+            {
+                throw new Exception("this is me");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        static void Method2()
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception ex)
+            {
+                throw ex; //throw
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Method2();
+            }
+            catch(Exception ex)
+            { 
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+ 
